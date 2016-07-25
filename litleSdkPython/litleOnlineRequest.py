bbe4cde6f10ef94f4981a582fb9747a0631a2797
Oleksandr Yemets <Oleksandr.Yemets@castingnetworks.com>
2016-07-25 14:11:59 +0300

    add requests instead of urllib

--- litleSdkPython/litleOnlineRequest.py
+++ litleSdkPython/litleOnlineRequest.py
@@ -1,32 +1,33 @@
-#Copyright (c) 2011-2012 Litle & Co.
-#
-#Permission is hereby granted, free of charge, to any person
-#obtaining a copy of this software and associated documentation
-#files (the "Software"), to deal in the Software without
-#restriction, including without limitation the rights to use,
-#copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the
-#Software is furnished to do so, subject to the following
-#conditions:
-#
-#The above copyright notice and this permission notice shall be
-#included in all copies or substantial portions of the Software.
-#
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-#EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
-#OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-#NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
-#HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
-#WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
-#FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-#OTHER DEALINGS IN THE SOFTWARE.
+"""
+Copyright (c) 2011-2012 Litle & Co.
 
-#import litleSdkPython
+Permission is hereby granted, free of charge, to any person
+obtaining a copy of this software and associated documentation
+files (the "Software"), to deal in the Software without
+restriction, including without limitation the rights to use,
+copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following
+conditions:
+
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+"""
+
 from . import litleXmlFields
 import pyxb
-import os
 from .Communications import *
 from .Configuration import *
+
 
 class litleOnlineRequest:
 
@@ -39,32 +40,36 @@ class litleOnlineRequest:
         self.communications = Communications(self.Configuration)
         self.printXml = Configuration.printXml
 
-    def _litleToXml(self,litleOnline):
-        try :
+    def _litleToXml(self, litleOnline):
+        try:
             dom = litleOnline.toDOM()
-            temp = dom.toxml('utf-8')
-            temp= temp.replace('ns1:','')
-            return temp.replace(':ns1','')
+            # temp = dom.toxml('utf-8')
+            temp = dom.toxml()
+            temp = temp.replace('ns1:', '')
+            return temp.replace(':ns1', '')
         except pyxb.ValidationError as e:
-            raise Exception("Invalid Number of Choices, Fill Out One and Only One Choice",e)
+            raise Exception(
+                "Invalid Number of Choices, Fill Out One and Only One Choice",
+                e)
 
-    def sendRequest(self,transaction, user=None, password=None, version=None, merchantId=None, reportGroup=None,
-                    timeout=None, url=None, proxy=None):
-        if (user != None):
+    def sendRequest(self, transaction, user=None, password=None, version=None,
+                    merchantId=None, reportGroup=None, timeout=None,
+                    url=None, proxy=None):
+        if (user is not None):
             self.User = user
-        if (password != None):
+        if (password is not None):
             self.Password = password
-        if (merchantId != None):
+        if (merchantId is not None):
             self.MerchantId = merchantId
-        if (reportGroup != None):
+        if (reportGroup is not None):
             self.ReportGroup = reportGroup
 
         litleOnline = self._createTxn(transaction)
         requestXml = self._litleToXml(litleOnline)
         if(self.printXml):
             print('\nRequest:\n', requestXml)
-        responseXml = self.communications.http_post(requestXml, url=url,
-                                                    proxy=proxy, timeout=timeout)
+        responseXml = self.communications.http_post(
+            requestXml, url=url, proxy=proxy, timeout=timeout)
         if(self.printXml):
             print('\nResponse:\n', responseXml)
         return self._processResponse(responseXml)
@@ -79,7 +84,7 @@ class litleOnlineRequest:
         litleOnline.merchantSdk = '9.3.2'
         authentication = litleXmlFields.authentication()
         authentication.user = self.User
-        authentication.password =  self.Password
+        authentication.password = self.Password
         litleOnline.authentication = authentication
         transaction.reportGroup = self.ReportGroup
         if isinstance(transaction, litleXmlFields.recurringTransactionType):
@@ -90,18 +95,19 @@ class litleOnlineRequest:
 
     def _addNamespace(self, responseXml):
         if ((responseXml.count("xmlns='http://www.litle.com/schema'") == 0) and
-            (responseXml.count('xmlns="http://www.litle.com/schema"') == 0)):
-            return responseXml.replace(' response=',' xmlns="http://www.litle.com/schema" response=')
+                (responseXml.count(
+                    'xmlns="http://www.litle.com/schema"') == 0)):
+            return responseXml.replace(
+                ' response=', ' xmlns="http://www.litle.com/schema" response=')
         return responseXml
 
     def _processResponse(self, responseXml):
         temp = self._addNamespace(responseXml)
         try:
-            response =litleXmlFields.CreateFromDocument(temp)
+            response = litleXmlFields.CreateFromDocument(temp)
         except Exception as e:
             raise Exception("Error Processing Response", e)
         if (response.response == '0'):
             return response.transactionResponse
         else:
             raise Exception(response.message)
-
--- setup.py
+++ setup.py
@@ -11,7 +11,8 @@ setup(name='LitleSdkPython',
       packages=['litleSdkPython'],
       install_requires=[
                         'PyXB==1.2.4',
-                        'paramiko==1.14.0'],
+                        'paramiko==1.14.0',
+                        'requests==2.10.0'],
       classifiers=[
           'Development Status :: 5 - Production/Stable',
           'Environment :: Console',
