babc09722dc714c1ae46de097995c987eee32452
Oleksandr Yemets <oleksandr.yemets@castingnetworks.com>
2016-08-11 14:40:38 +0300

    add examples in process

---
+++ examples/exampleConfig.py
@@ -0,0 +1,42 @@
+from litleSdkPython.litleOnlineRequest import Configuration, litleXmlFields
+
+
+def set_config():
+    config = Configuration()
+    config.username = "jenkins"
+    config.password = "certpass"
+    config.merchantId = "0180"
+    config.url = "Sandbox"
+    config.proxy = "iwp1.lowell.litle.com:8080"
+    # Use it to display all request and response body
+    config.printXml = True
+
+    return config
+
+
+def set_card():
+    card = litleXmlFields.cardType()
+    card.number = "4457010000000009"
+    card.expDate = "0112"
+    card.cardValidationNum = "349"
+    card.type = "VI"
+
+    return card
+
+
+def set_contact():
+    contact = litleXmlFields.contact()
+    contact.name = "John Smith"
+    contact.addressLine1 = "1 Main St."
+    contact.city = "Burlington"
+    contact.state = "MA"
+    contact.zip = "01803-3747"
+    contact.country = "USA"
+
+    return contact
+
+
+def display_results(response):
+    print("Response: " + response.response)
+    print("Message: " + response.message)
+    print("LitleTransaction ID: " + str(response.litleTxnId))
---
+++ examples/sales_and_void.py
@@ -0,0 +1,58 @@
+import exampleConfig
+from litleSdkPython.litleOnlineRequest import litleOnlineRequest, litleXmlFields
+
+config = exampleConfig.set_config()
+card = exampleConfig.set_card()
+contact = exampleConfig.set_contact()
+void = litleXmlFields.void()
+
+# Set the sale with card
+sale = litleXmlFields.sale()
+sale.orderId = "2"
+sale.amount = 10010
+sale.orderSource = "ecommerce"
+sale.billToAddress = contact
+sale.card = card
+
+litleXml = litleOnlineRequest(config)
+response = litleXml.sendRequest(sale)
+
+print('\n')
+print('Send sale with card request:')
+exampleConfig.display_results(response)
+
+# Void current sale
+import ipdb; ipdb.set_trace() # DEBUG
+void.litleTxnId = response.litleTxnId
+response = litleXml.sendRequest(void)
+
+print('\n')
+print('Void current sale request')
+exampleConfig.display_results(response)
+
+# Set the sale with token and clear card state first
+sale.card = None
+token = litleXmlFields.cardTokenType()
+token.litleToken = "1234123412341234"
+token.expDate = "0112"
+token.cardValidationNum = "349"
+token.type = "VI"
+
+sale.token = token
+
+response = litleXml.sendRequest(sale)
+
+print('\n')
+print('Send sale with token request')
+exampleConfig.display_results(response)
+
+# Void current sale
+void.litleTxnId = response.litleTxnId
+response = litleXml.sendRequest(void)
+
+print('\n')
+print('Void current sale request')
+exampleConfig.display_results(response)
+
+if response.response != "000":
+    raise Exception("Incorrect response")
